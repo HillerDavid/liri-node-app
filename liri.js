dd8b25575780b6160d798679867dcb5c0d02c945
David Hiller <HillerDavid@users.noreply.github.com>
2018-10-20 20:08:42 -0400

    set up case for spotify and can now take multiple words in search

--- liri.js
+++ liri.js
@@ -9,21 +9,39 @@ const SPOTIFY = require('node-spotify-api');
 
 let spotify = new SPOTIFY(keys.spotify);
 
-let getArtistNames = (artist) => {
-    return artist.name
+function spotifySearch(songName) {
+    let getArtistNames = (artist) => {
+        return artist.name
+    }
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        let songs = data.tracks.items
+        for (let i = 0; i < songs.length; i++) {
+            console.log(i)
+            console.log(`Artist(s): ${songs[i].artists.map(getArtistNames)}`)
+            console.log(`Song Title: ${songs[i].name}`)
+            console.log(`Preview Song: ${songs[i].preview_url}`)
+            console.log(`Album: ${songs[i].album.name}`)
+            console.log(`=================================================`)
+        }
+        // console.log(data.tracks.items[0]);
+    });
 }
-spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
+function option(command, functionData) {
+    switch (command) {
+        case 'spotify-this-song':
+            spotifySearch(functionData)
+            break;
+        default:
+            console.log('Not a liri function')
+            break;
     }
-    let songs = data.tracks.items
-    for (let i = 0; i < songs.length; i++) {
-        console.log(i)
-        console.log(`Artist(s): ${songs[i].artists.map(getArtistNames)}`)
-        console.log(`Song Title: ${songs[i].name}`)
-        console.log(`Preview Song: ${songs[i].preview_url}`)
-        console.log(`Album: ${songs[i].album.name}`)
-        console.log(`=================================================`)
-    }
-    // console.log(data.tracks.items[0]);
-});
+}
+
+function runProgram (command, functionData) {
+    option(command, functionData)
+}
+
+runProgram(process.argv[2], process.argv.slice(3, process.argv.length))

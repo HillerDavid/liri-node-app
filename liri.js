fdb0d58c0142ed23ac49e14ab8a4da7657c8b172
David Hiller <HillerDavid@users.noreply.github.com>
2018-10-23 00:58:49 -0400

    movie-this function added to liri

--- liri.js
+++ liri.js
@@ -23,6 +23,7 @@ function spotifySearch(songName) {
         }
         let songs = data.tracks.items
         for (let i = 0; i < songs.length; i++) {
+            console.log(`=================================================`)
             console.log(i)
             console.log(`Artist(s): ${songs[i].artists.map(getArtistNames)}`)
             console.log(`Song Title: ${songs[i].name}`)
@@ -36,17 +37,47 @@ function spotifySearch(songName) {
 //Search Bands In Town for concerts
 function bandsInTownSearch(artistName) {
     REQUEST(`https://rest.bandsintown.com/artists/${artistName}/events?app_id=${keys.bandsintown.id}`, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // Print the error if one occurred
+        // console.log('error:', error)
+        // Print the response status code if a response was received
+        // console.log('statusCode:', response && response.statusCode)
         let concerts = JSON.parse(body)
-        console.log(concerts)
         for (let i = 0; i < concerts.length; i++) {
+            console.log(`=================================================`)
+            console.log(i)
             console.log(`Venue: ${concerts[i].venue.name}`)
             console.log(`Location: ${concerts[i].venue.city}, ${concerts[i].venue.region}`)
             let date = MOMENT(concerts[i].datetime, MOMENT.ISO_8601)
             console.log(`Date: ${MOMENT(date).format('MM/DD/YYYY')}`)
+            console.log(`=================================================`)
         }
     });
+}
+
+//Search OMDB for movies
+function omdbSearch(movieTitle) {
+    REQUEST(`http://www.omdbapi.com/?t=${movieTitle}&apikey=9641550c`, function (error, response, body) {
+        // Print the error if one occurred
+        // console.log('error:', error)
+        // Print the response status code if a response was received
+        // console.log('statusCode:', response && response.statusCode)
+        let movie = JSON.parse(body)
+        console.log(`=================================================`)
+        console.log(`Title: ${movie.Title}`)
+        console.log(`Release Year: ${movie.Year}`)
+        console.log(`IMDB Rating: ${movie.Ratings[0].Value}`)
+        console.log(`Rotten Tomatoes Rating: ${movie.Ratings[1].Value}`)
+        console.log(`Produced in: ${movie.Country}`)
+        console.log(`Language: ${movie.Language}`)
+        console.log(`Plot: ${movie.Plot}`)
+        console.log(`Actors: ${movie.Actors}`)
+        console.log(`=================================================`)
+    });
+}
+
+//Perform task stored in random.txt
+function doWhatItSays() {
+    
 }
 
 //Checks and runs search user input
@@ -57,6 +88,12 @@ function option(command, data) {
             break
         case 'concert-this':
             bandsInTownSearch(data)
+            break
+        case 'movie-this':
+            omdbSearch(data)
+            break
+        case 'do-what-it-says':
+            doWhatItSays()
             break
         default:
             console.log('Please enter a LIRI function')

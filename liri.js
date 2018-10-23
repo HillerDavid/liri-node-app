98f8471da74f968b7a8f51c15f0b399e67172a43
David Hiller <HillerDavid@users.noreply.github.com>
2018-10-22 22:39:26 -0400

    Added bandsInTownSearch functionality. Date display needs to be formatted

--- liri.js
+++ liri.js
@@ -1,21 +1,25 @@
+//Configure dotenv package
 require('dotenv').config()
 
-// const spotify = require('node-spotify-api')
-// const request = require('request')
-// const moment = require('moment')
-
+//Import keys.js file
 let keys = require('./keys.js')
+
+//Constants for require imports
+const REQUEST = require('request')
+const MOMENT = require('moment')
 const SPOTIFY = require('node-spotify-api');
 
+//Create new SPOTIFY object with keys
 let spotify = new SPOTIFY(keys.spotify);
 
+//Search spotify 
 function spotifySearch(songName) {
     let getArtistNames = (artist) => {
         return artist.name
     }
-    spotify.search({ type: 'track', query: songName }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
+    spotify.search({ type: 'track', query: songName }, function (error, data) {
+        if (error) {
+            return console.log('Error occurred: ' + error);
         }
         let songs = data.tracks.items
         for (let i = 0; i < songs.length; i++) {
@@ -26,22 +30,40 @@ function spotifySearch(songName) {
             console.log(`Album: ${songs[i].album.name}`)
             console.log(`=================================================`)
         }
-        // console.log(data.tracks.items[0]);
     });
 }
-function option(command, functionData) {
+
+function bandsInTownSearch(artistName) {
+    REQUEST(`https://rest.bandsintown.com/artists/${artistName}/events?app_id=${keys.bandsintown.id}`, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        let concert = JSON.parse(body)
+
+        console.log(`Venue: ${JSON.stringify(concert[0].venue.name)}`)
+        console.log(`Location: ${JSON.stringify(concert[0].venue.city)}, ${JSON.stringify(concert[0].venue.region)}`)
+        console.log(`Date: ${JSON.stringify(concert[0].datetime)}`)
+    });
+}
+
+//Checks and runs search user input
+function option(command, data) {
     switch (command) {
         case 'spotify-this-song':
-            spotifySearch(functionData)
-            break;
+            spotifySearch(data)
+            break
+        case 'concert-this':
+            bandsInTownSearch(data)
+            break
         default:
-            console.log('Not a liri function')
-            break;
+            console.log('Please enter a LIRI function')
+            break
     }
 }
 
-function runProgram (command, functionData) {
-    option(command, functionData)
+//
+function runProgram(command, data) {
+    option(command, data)
 }
 
-runProgram(process.argv[2], process.argv.slice(3, process.argv.length))
+//
+runProgram(process.argv[2], process.argv.slice(3, process.argv.length).toString().replace(",", "+"))
